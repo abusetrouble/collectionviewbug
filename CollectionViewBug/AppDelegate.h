2e600a715d0eafbe499db25b5af94fe0bc90e9cd
abusetrouble <artem.zolotuhin@gmail.com>
2017-07-31 13:03:42 +0400

    Initial Commit

---
+++ CollectionViewBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewBug
+//
+//  Created by Artem Zolotuhin on 31.07.17.
+//  Copyright © 2017 artemz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewBug
+//
+//  Created by Artem Zolotuhin on 31.07.17.
+//  Copyright © 2017 artemz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
